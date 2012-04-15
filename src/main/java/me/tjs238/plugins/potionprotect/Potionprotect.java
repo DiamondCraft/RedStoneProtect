1968a27d46479c66206d6c29a87145e9bcdc55ee
Tjs238 <washington@xploitedgaming.com>
2012-04-14 23:24:35 -0400

    Adding Item drop event

--- src/main/java/me/tjs238/plugins/potionprotect/Potionprotect.java
+++ src/main/java/me/tjs238/plugins/potionprotect/Potionprotect.java
@@ -1,22 +1,46 @@
 package me.tjs238.plugins.potionprotect;
 
+import java.util.logging.Logger;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PotionSplashEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Potionprotect extends JavaPlugin implements Listener {
+    public Server server = Bukkit.getServer();
+    private Logger log;
+    private PluginDescriptionFile description;
+    private String prefix;
+    
     public void onDisable() {
         // TODO: Place any custom disable code here.
     }
 
     public void onEnable() {
+        log = Logger.getLogger("Minecraft");
+        description = getDescription();
+        prefix = "["+description.getName()+"] ";
+        log("Starting up...");
         getServer().getPluginManager().registerEvents(this, this);
+        
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        event.getPlayer().sendMessage("Welcome, " + event.getPlayer().getDisplayName() + "!");
+    public void onItemDrop(PlayerDropItemEvent event) {
+        Player player = event.getPlayer();
+        Item item = event.getItemDrop();
+        
+    }
+    
+    public void log(String message){
+        log.info(prefix+message);
     }
 }
 
