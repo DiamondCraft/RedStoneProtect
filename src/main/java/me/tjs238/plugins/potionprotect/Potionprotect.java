461ea2612d1e0feeb4771b16c35e2526e17d97c6
Tjs238 <washington@xploitedgaming.com>
2012-04-15 00:59:15 -0400

    Finnishing coding

--- src/main/java/me/tjs238/plugins/potionprotect/Potionprotect.java
+++ src/main/java/me/tjs238/plugins/potionprotect/Potionprotect.java
@@ -1,10 +1,22 @@
 package me.tjs238.plugins.potionprotect;
 
+import com.sk89q.minecraft.util.commands.CommandException;
+import com.sk89q.worldedit.IncompleteRegionException;
+import com.sk89q.worldedit.Vector;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.bukkit.selections.Selection;
+import com.sk89q.worldedit.regions.CuboidRegion;
+import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.regions.RegionSelector;
+import com.sk89q.worldedit.regions.*;
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Server;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -31,26 +43,44 @@ public class Potionprotect extends JavaPlugin implements Listener {
         prefix = "["+description.getName()+"] ";
         log("Starting up...");
         getServer().getPluginManager().registerEvents(this, this);
-        
     }
 
     @EventHandler
-    public void onItemDrop(PlayerDropItemEvent event) {
+    public void onItemDrop(PlayerDropItemEvent event) throws IncompleteRegionException {
         if (event.isCancelled()) {
             return;
         }
         Player player = event.getPlayer();
         Item item = event.getItemDrop();
+        WorldGuardPlugin worldGuard = (WorldGuardPlugin) getServer().getPluginManager().getPlugin("WorldGuard");
+        WorldEditPlugin worldEdit = (WorldEditPlugin) getServer().getPluginManager().getPlugin("WorldEdit");
         
         if (item.equals(Material.POTION)) {
             event.setCancelled(true);
-            Location init = player.getLocation();
-            double x1 = init.getX();
-            double z1 = init.getZ();
-            double y = init.getY();
-            Location pos1 = init.add(x1 + 10, y, z1 + 10);
-            Location pos2 = init.add(x1 - 10, y, z1 + 10);
-            // TODO: Add World Guard Dependancy
+            Vector vector = new Vector();
+            CuboidRegionSelector selector = new CuboidRegionSelector();
+            Location pos1 = player.getLocation().add(10, 0, 10);
+            Location pos2 = player.getLocation().subtract(10, 0, 10);
+            Vector vpos1 = vector.add(pos1.getX(), pos1.getY(), pos1.getZ());
+            Vector vpos2 = vector.add(pos2.getX(), pos2.getY(), pos2.getZ());
+            selector.selectPrimary(vpos1);
+            selector.selectSecondary(vpos2);
+            
+            Selection sel = worldEdit.getSelection(player);
+            int oldSize = sel.getArea();
+            Region region = sel.getRegionSelector().getRegion();
+            try {
+                sel.getRegionSelector().getRegion().expand(
+                        new Vector(0, (player.getWorld().getMaxHeight() + 1), 0),
+                        new Vector(0, (player.getWorld().getMaxHeight() + 1), 0));
+                
+                // TODO: Add World Guard Dependancy
+            } catch (RegionOperationException ex) {
+                Logger.getLogger(Potionprotect.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            BlockVector
+            
         }
     }
     
