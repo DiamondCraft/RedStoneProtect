ef966afc1d3b81678e13da7df77b7793cfe2d62c
Tjs238 <washington@xploitedgaming.com>
2012-04-18 21:48:20 -0400

    Passed the region. Need to fix Protected Region error

--- src/main/java/me/tjs238/plugins/potionprotect/Potionprotect.java
+++ src/main/java/me/tjs238/plugins/potionprotect/Potionprotect.java
@@ -11,7 +11,9 @@ import com.sk89q.worldedit.regions.Region;
 import com.sk89q.worldedit.regions.RegionSelector;
 import com.sk89q.worldedit.regions.*;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
+import com.sk89q.worldguard.protection.regions.ProtectedPolygonalRegion;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import java.util.Random;
 import java.util.logging.Level;
@@ -28,6 +30,8 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.PotionSplashEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -69,9 +73,9 @@ public class Potionprotect extends JavaPlugin implements Listener, ConversationA
 
     public void conversationAbandoned(ConversationAbandonedEvent cae) {
         if (cae.gracefulExit()) {
-            cae.getContext().getForWhom().sendRawMessage("Conversation exited gracefully");
+            cae.getContext().getForWhom().sendRawMessage(ChatColor.AQUA+"Closing Builder");
         } else {
-            cae.getContext().getForWhom().sendRawMessage(("Conversation abandoned by"+ cae.getCanceller().getClass().getName()));
+            cae.getContext().getForWhom().sendRawMessage(ChatColor.AQUA+"Builder Closed Forcably!");
         }
     }
     
@@ -95,16 +99,16 @@ public class Potionprotect extends JavaPlugin implements Listener, ConversationA
     
     private class AreYouSurePrompt extends FixedSetPrompt {
         public AreYouSurePrompt() {
-            super("Yes", "No");
+            super("Yes", "No", "yes", "no");
         }
         
         public String getPromptText(ConversationContext context) {
-            return "Are you sure you want to make this region? "+formatFixedSet();
+            return "Are you sure you want to make this region? [Yes, No]";
         }
         
         @Override
         protected Prompt acceptValidatedInput(ConversationContext context, String s) {
-            if (s.equals("No")) {
+            if (s.equals("No") || s.equals("no")) {
                 return Prompt.END_OF_CONVERSATION;
             }
             context.setSessionData("agreed", s);
@@ -137,7 +141,16 @@ public class Potionprotect extends JavaPlugin implements Listener, ConversationA
         public String getPromptText(ConversationContext context) {
             Player player = (Player)context.getSessionData("who");
             String type = (String)context.getSessionData("type");
-            
+            try {
+                if (player != null)
+                createRegion(player, type);
+                else
+                Logger.getLogger(Potionprotect.class.getName()).log(Level.SEVERE, null, "Player is null!");
+            } catch (RegionOperationException ex) {
+                Logger.getLogger(Potionprotect.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IncompleteRegionException ex) {
+                Logger.getLogger(Potionprotect.class.getName()).log(Level.SEVERE, null, ex);
+            }
             return "Still working on this!!!";
         }
         
@@ -153,12 +166,12 @@ public class Potionprotect extends JavaPlugin implements Listener, ConversationA
             Player who = (Player)context.getSessionData("who");
             
             if (what != null && who == null) {
-                return ChatColor.AQUA+"DCR-Region:"+what+": ";
+                return ChatColor.AQUA+"[DCR]Region-Size:"+what+": ";
             }
-            if (what != null && who == null) {
-                return ChatColor.AQUA+"DCR-Region:"+what+"-Player: "+who+": ";
+            if (what != null && who != null) {
+                return ChatColor.AQUA+"[DCR]Region-Size:"+what+" Player: "+who.getDisplayName()+": ";
             }
-            return ChatColor.AQUA+"DCR: ";
+            return ChatColor.AQUA+"[DCR] ";
         }
     }
     
@@ -170,6 +183,14 @@ public class Potionprotect extends JavaPlugin implements Listener, ConversationA
             return null; //throws a NullPointerException, telling the Admin that WG is not loaded.
         }
         return (WorldGuardPlugin)plugin;
+    }
+    
+    public WorldEditPlugin getWorldEdit() {
+        Plugin plugin = getServer().getPluginManager().getPlugin("WorldEdit");
+        if ((plugin == null) || (!(plugin instanceof WorldEditPlugin))) {
+            return null;
+        }
+        return (WorldEditPlugin)plugin;
     }
     
     public Vault getVault() {
@@ -186,13 +207,70 @@ public class Potionprotect extends JavaPlugin implements Listener, ConversationA
         Player player = event.getPlayer();
         
         if(block.getType().equals(Material.REDSTONE_ORE)) {
-            if (player instanceof Conversable) {
+            if (player instanceof Conversable && player.hasPermission("dc.protect.block")) {
+                player.sendMessage(ChatColor.AQUA+"[DCR]Starting Region Build...");
                 conversationFactory.buildConversation((Conversable)player).begin();
+                event.setCancelled(true);
+                ItemStack redstone = new ItemStack(Material.REDSTONE_ORE, 1);
+                Inventory inv = player.getInventory();
+                inv.remove(redstone);
             } else {
                 player.sendMessage(ChatColor.RED+"Error: 1908");
                 player.sendMessage(ChatColor.RED+"Please contact Staff!");
             }
         }
+    }
+    
+    @SuppressWarnings("deprecation")
+    public void createRegion(Player player, String size) throws RegionOperationException, IncompleteRegionException {
+        Vector vector = new Vector();
+        //WorldEditPlugin worldEdit = (WorldEditPlugin)getServer().getPluginManager().getPlugin("WorldEdit");
+        //WorldGuardPlugin worldGuard = (WorldGuardPlugin)getServer().getPluginManager().getPlugin("WorldGuard");
+        log("Getting WorldGuard");
+        WorldGuardPlugin worldGuard = getWorldGuard();
+        log("Getting WorldEdit");
+        WorldEditPlugin worldEdit = getWorldEdit();
+        CuboidRegionSelector selector = new CuboidRegionSelector();
+        log("Setting the points");
+        Location pos1 = player.getLocation().add(10, 0 ,10);
+        Location pos2 = player.getLocation().subtract(10, 0, 10);
+        log("Setting the vectors");
+        Vector vpos1 = vector.add(pos1.getBlockX(), pos1.getBlockY(), pos1.getBlockZ());
+        Vector vpos2 = vector.add(pos2.getBlockX(), pos2.getBlockY(), pos2.getBlockZ());
+        log("Selecting primary");
+        selector.selectPrimary(vpos1);
+        log("Selecting secondary");
+        selector.selectSecondary(vpos2);
+        log("Learning changes");
+        selector.learnChanges();
+        log("Getting the selection above");
+        if (player == null) {
+            return;
+        }
+            log("Making it top to bottom");
+                selector.getRegion().expand(
+                            new Vector(0, (player.getWorld().getMaxHeight() +1), 0),
+                            new Vector(0, (player.getWorld().getMaxHeight() - 1), 0)
+                            );
+                log("Learning that");
+                selector.learnChanges();
+            
+                log("Setting block vectors again");
+        BlockVector min = selector.getRegion().getPos1().toBlockVector();
+        BlockVector max = selector.getRegion().getPos2().toBlockVector();
+        RegionManager rm = worldGuard.getGlobalRegionManager().get(player.getWorld());
+        log("Getting random number");
+        Random rand = new Random();
+        ProtectedRegion region;
+        int range1 = rand.nextInt(20);
+        if (player.hasPermission("dc.protect")) {
+            log("Setting region");
+            String rname = player.getName()+"_"+range1;
+            region = new ProtectedCuboidRegion(rname, min, max);
+            rm.addRegion(region);
+            log("Region created with name: "+rname);
+        }
+        
     }
 
     /*@EventHandler
